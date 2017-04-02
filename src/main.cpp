44da278aadb6382550bdb90d4ece6d9ac04d45db
Lucas Alexandre Soares <lucassoares1793@gmail.com>
2017-04-02 17:10:52 -0300

    add group members and headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+/* Main source file - Homework assignment 1 - moving objects with OpenGL & GLUT
+
+	Lucas Alexandre Soares 		-	9293265
+	Rafael Jureg Monteiro 		-	9293095
+	Giovanna Oliveira Guimarães -	9293693
+	
+*/
+
 #include <GL/glut.h>
 
 int main(int argc, char* argv[]){
