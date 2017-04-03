932cf40c2acc3a90299b1ed2d8450cb447544a13
Lucas Alexandre Soares <lucassoares1793@gmail.com>
2017-04-03 15:30:28 -0300

    commit

--- src/callback.cpp
+++ src/callback.cpp
@@ -7,6 +7,7 @@
 */
 
 #include <GL/glut.h>
+
 #include "callback.hpp"
 #include "graphics.hpp"
 
@@ -14,21 +15,35 @@ void Draw(){
 
 	ClearScreen();
 
-	
+
 
 	glFlush();
 }
 
-void KeyboardHandle(unsigned char, int, int ){
+/* KeyboardHandle
 
-}
+	uint8 key 	- Which keyboard key was pressed
+	int x 		- Mouse pointer x coordinate
+	int y 		- Mouse pointer y coordinate
+*/
+void KeyboardHandle(uint8 key, int x, int y){ /* TODO */}
 
-void MouseHandle(int, int, int, int){
+/* MouseHandle
+
+	int button 	- Which mouse button was clicked (LEFT_CLICK, RIGHT_CLICK, 
+				  MIDDLE_CLICK)
+	int state 	- The button stat (BUTTON_DOWN or BUTTON_UP)
+	int x 		- Screen x coordinate
+	int y 		- Screen y coordinate
+*/
+void MouseHandle(int button, int state, int x, int y){
 
 }
 
 void ResizeHandle(int width, int height){
 
-	// glViewport(0, 0, width, height);
-
+	glViewport(0, 0, width, height);
+	gluOrtho2D(0.0f, (float) width, 0.0f, (float) height);
+	ClearScreen(1.0f, 1.0f, 1.0f, 1.0f);
+	glFlush();
 }
