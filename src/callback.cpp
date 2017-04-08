00ec2c981214e2c001a99348479c7f66165cf6ec
rafaelmonteiro95 <rafaelmonteiro95@gmail.com>
2017-04-08 01:47:35 -0300

    vamo deixar de ser burro neh jureg

--- src/callback.cpp
+++ src/callback.cpp
@@ -14,30 +14,56 @@
 
 using namespace std;
 
-float dest_x = 90;
-float dest_y = 90;
+float destx = 0;
+float desty = 0;
 
-float curx = 90;
-float cury = 90;
+float objx[3] = {25.0f, 50.0f, 75.0f};
+float objy[3] = {50.0f, 50.0f, 50.0f};
 
-void drawTriangle(){
-	glBegin(GL_TRIANGLES);
-		glColor3f(1.0f, 0.0f, 0.0f); // Define a cor vermelha para um dos vértices
-		glVertex2f(10.f, 10.0f);
+float stepx[3] = {0, 0, 0};
+float stepy[3] = {0, 0, 0};
 
-		glColor3f(0.0f, 1.0f, 0.0f); // Define a cor verde para um dos vértices
-		glVertex2f(90.0f, 10.0f);
+float speed[3] = {0.001, 0.003, 0.005};
 
-		glColor3f(0.0f, 0.0f, 1.0f); // Define a cor azul para um dos vértices
-		glVertex2f(curx, cury);
-	glEnd();
+void drawTriangles(){
+
+	//for each polygon, draws it in the correct position using translation
+	for(int i = 0; i < 3; i++){
+		
+		//pushing previous matrix
+		glPushMatrix();
+
+		//translates the new matrix one step closer to dest
+		glTranslatef(objx[i] + stepx[i],
+					 objy[i] + stepy[i],
+			 		 0.0f);
+
+		//draws the polygon
+		glBegin(GL_TRIANGLES);
+			glColor3f(1.0f, 0.0f, 0.0f); // Define a cor vermelha para um dos vértices
+			glVertex2f(0.0f, 5.0f);
+
+			glColor3f(0.0f, 1.0f, 0.0f); // Define a cor verde para um dos vértices
+			glVertex2f(-5.0f, -5.0f);
+
+			glColor3f(0.0f, 0.0f, 1.0f); // Define a cor azul para um dos vértices
+			glVertex2f(5.0f, -5.0f);
+		glEnd();
+
+		//updates the object position
+		objx[i] += stepx[i];
+		objy[i] += stepy[i];
+
+		//returns to the default matrix
+		glPopMatrix();
+	}
 }
 
 void Draw(){
-	
+
 	ClearScreen();
 
-	drawTriangle();
+	drawTriangles();
 
 	glFlush();
 }
@@ -60,14 +86,21 @@ void KeyboardHandle(unsigned char key, int x, int y){
 	int x 		- Screen x coordinate
 	int y 		- Screen y coordinate
 */
-void MouseHandle(int button, int state, int xx, int yy){
+void MouseHandle(int button, int state, int x, int y){
 	if(button == GLUT_LEFT_BUTTON && state == GLUT_UP){
 		float winx = glutGet(GLUT_WINDOW_WIDTH);
 		float winy = glutGet(GLUT_WINDOW_HEIGHT);
 		
+		//calculating screen position of click
+		destx = 100*(((float)x)/winx);
+		desty = 100-(100*(((float)y)/winy));
 
-		dest_x = 100*(((float)xx)/winx);
-		dest_y = 100-(100*(((float)yy)/winy));
+		//sets the new step size for each polygon 
+		for(int i = 0; i < 3; i++){
+			stepx[i] = (destx - objx[i]) * speed[i];
+			stepy[i] = (desty - objy[i]) * speed[i];		
+			
+		}
 	}
 }
 
@@ -87,23 +120,19 @@ void ResizeHandle(int width, int height){
 	// glFlush();
 }
 
-float step = 0.1;
 
 void IdleHandle(){
 
-	if(curx < dest_x){
-		curx += step;
-	}
-	if(curx > dest_x){
-		curx -= step;
-	}
-
-	if(cury < dest_y){
-		cury += step;
-	}
-	if(cury > dest_y){
-		cury -= step;
+	//checking if the polygons are out of the screen
+	for(int i = 0; i < 3; i++){
+		if(objx[i] > 100 || objx[i] < 0 || objy[i] > 100 || objy[i] < 0){
+			stepx[i] = 0.0f;
+			stepy[i] = 0.0f;
+			objx[i] = (25.0f)*(i+1);
+			objy[i] = 50.0f;
+		}
 	}
 
+	//draws changes
 	glutPostRedisplay();
 }
