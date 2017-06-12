0424c71af93a7f38e0a19c599b0b77dcdccebd24
Lucas Alexandre Soares <lucassoares1793@gmail.com>
2017-06-12 20:04:39 -0300

    start working on lightning

--- trab3/src/callbacks.cpp
+++ trab3/src/callbacks.cpp
@@ -77,6 +77,25 @@ Camera* cam;
 //list of scene objects.
 sceneObject objects[3];
 
+void InitLightning(){
+	
+	// OpenGL lightning
+	float angle[] = {1,1,0};
+	float ambient[] = {1,1,1,1};
+	float diffuse[] = {1,1,1,1};
+	float specular[] = {1,1,1,1};
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, angle);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, angle);
+	// glLightfv(GL_LIGHT0, GL_DIFFUSE, angle);
+	// glLightfv(GL_LIGHT0, GL_SPECULAR, angle);
+    
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+}
+
 void myInit(){
 
 	cam = new Camera();
@@ -98,7 +117,10 @@ void myInit(){
 								0.0f, 0.0f, 0.0f,
 								0.0f, 0.0f, 0.0f);
 	objects[2].type = CUBE;
+
+	InitLightning();
 }
+
 
 // function that deletes "camera" and "sceneObject" objects
 void myCleanup(){
